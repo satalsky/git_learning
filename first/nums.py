8c18bf50ef01653ae2de6320ab5fadb820091c9b
BuildTools <unconfigured@null.spigotmc.org>
2022-02-20 20:50:51 +0900

    Added module strings.py

--- first/nums.py
+++ first/nums.py
@@ -1,10 +1,16 @@
 def plus(a, b):
     return a + b
 
-def minus(a,b):
+
+def minus(a, b):
     return a - b
+
+
+def mul(a, b):
+    return a * b
 
 
 if __name__ == "__main__":
     print(plus(2, 2))
     print(minus(4, 2))
+    print(mul(2, 2))
